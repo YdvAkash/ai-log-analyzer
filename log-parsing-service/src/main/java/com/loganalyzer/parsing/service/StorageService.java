c0bb42a980e6be88881e74ecfcab551e39d1bc0c
Akash Yadav <136945099+YdvAkash@users.noreply.github.com>
2026-02-15 16:43:14 +0530

    commit micro 1.4

--- log-parsing-service/src/main/java/com/loganalyzer/parsing/service/StorageService.java
+++ log-parsing-service/src/main/java/com/loganalyzer/parsing/service/StorageService.java
@@ -27,13 +27,14 @@ public class StorageService {
             log.info("Attempting to save ID {} to MongoDB", entity.getId());
             mongoRepository.save(entity);
             log.info("Saved to MongoDB: {}", entity.getId());
-            
+
 com.loganalyzer.common.dto.LogEvent aiEvent = new com.loganalyzer.common.dto.LogEvent();
 aiEvent.setLogId(entity.getId());
 aiEvent.setServiceName(entity.getSource());
 aiEvent.setRawMessage(entity.getRawContent());
 aiEvent.setLogLevel(entity.getLogLevel());
 aiEvent.setClusterId(entity.getId()); // Testing ke liye ID ko hi ClusterId bana dein
+aiEvent.setTimestamp(entity.getTimestamp());
 
 kafkaTemplate.send("clustered-logs", entity.getId(), aiEvent);
 log.info("Properly mapped event sent to AI Service for Log ID: {}", entity.getId());
